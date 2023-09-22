95785e0d4b44867ee3044bb3004bbfcc33200313
TheODDYSEY <mosessabz07@gmail.com>
2023-09-22 22:53:04 +0300

    conversion of Pil image to pytorch tensor

--- image_seg.py
+++ image_seg.py
@@ -27,3 +27,7 @@ def load_image(image_path):
 img_path = "https://shorthaircatbreeds.com/wp-content/uploads/2020/06/Urban-cat-crossing-a-road-300x180.jpg"
 image = load_image(img_path)
 image
+# convert PIL Image to pytorch tensors
+transform = transforms.ToTensor()
+image_tensor = image.convert("RGB")
+image_tensor = transform(image_tensor)
