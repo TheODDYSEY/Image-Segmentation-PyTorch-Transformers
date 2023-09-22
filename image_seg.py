c52a51d2459170299a4a210baa940e121f8d0fa2
TheODDYSEY <mosessabz07@gmail.com>
2023-09-22 22:25:55 +0300

    Images Loading function

--- image_seg.py
+++ image_seg.py
@@ -7,3 +7,23 @@ import requests
 from PIL import Image
 import urllib.parse as parse
 import os
+
+# a function to determine whether a string is a URL or not
+def is_url(string):
+    try:
+        result = parse.urlparse(string)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
+
+# a function to load an image
+def load_image(image_path):
+    """Helper function to load images from their URLs or paths."""
+    if is_url(image_path):
+        return Image.open(requests.get(image_path, stream=True).raw)
+    elif os.path.exists(image_path):
+        return Image.open(image_path)
+
+img_path = "https://shorthaircatbreeds.com/wp-content/uploads/2020/06/Urban-cat-crossing-a-road-300x180.jpg"
+image = load_image(img_path)
+image
